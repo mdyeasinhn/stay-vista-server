70fbab0b1f2a273c1d0f3c672dd170ce35fa9d33
Md Yeasin <mdyeasinhn@gmail.com>
2024-08-30 19:37:32 +0600

    my listed delete api implement complete

--- index.js
+++ index.js
@@ -113,6 +113,14 @@ async function run() {
       res.send(result)
     });
 
+    // Delete a room
+    app.delete('/room/:id', async(req, res) => {
+      const id = req.params.id;
+      const query ={_id : new ObjectId(id)};
+      const result = await roomsCollection.deleteOne(query);
+      res.send(result )
+    })
+
 
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 })
