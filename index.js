a1af6d9a087e4486b5e8aab6511d26cf403df2ab
Md Yeasin <mdyeasinhn@gmail.com>
2024-09-23 17:47:31 +0600

    get all bookings for a guest

--- index.js
+++ index.js
@@ -251,6 +251,14 @@ async function run() {
 
     })
 
+    // get all bookings for a guest
+    app.get("/my-bookings/:email",verifyToken, async(req, res) =>{
+      const email = req.params.email;
+      const query = {"guest.email" : email}
+      const result = await bookingsCollection.find(query).toArray()
+      res.send(result);
+    })
+
 
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 })
